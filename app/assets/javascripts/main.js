c1ade6563c84e9b56e9e8213d7c7ffd9b665d72d
Joanna Colson <joannacolson@gmail.com>
2017-06-13 18:54:28 -0700

    fixed multiple google includes problem with app

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     console.log('jquery connected');
     $('.collapse').collapse()
 
+    // Moved this logic to the index page to prevent execution on all pages in main.js
     //Google Maps with many markers
     // console.log(JSON.parse(places));
-    var handler = Gmaps.build('Google');
-    handler.buildMap({ internal: { id: 'multi_markers' } }, function() {
-        var markers = handler.addMarkers(JSON.parse(places));
-        handler.bounds.extendWith(markers);
-        handler.fitMapToBounds();
-    });
+    // var handler = Gmaps.build('Google');
+    // handler.buildMap({ internal: { id: 'multi_markers' } }, function() {
+    //     var markers = handler.addMarkers(JSON.parse(places));
+    //     handler.bounds.extendWith(markers);
+    //     handler.fitMapToBounds();
+    // });
 });
